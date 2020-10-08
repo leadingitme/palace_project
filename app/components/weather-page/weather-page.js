a670658e8679b43f7e6eb2f7ef43d7bc7aa57341
Alex G <alex@leadingit.me>
2020-10-08 11:55:27 +0400

    initial commit

---
+++ app/components/weather-page/weather-page.js
@@ -0,0 +1,59 @@
+var weatherPageModule = (function () {
+  "use strict";
+
+  let outputValue = true;
+
+  /**
+   * Initialize Method
+   */
+  function onInit() {
+    serviceModule.addEmulatorScenarioBrowserOnly(
+      "./components/weather-page/weather-page-emulator.json"
+    );
+  }
+
+  /**
+   * Declare your Public Methods here
+   */
+  function getOutput() {
+    return outputValue;
+  }
+
+  // Methods for switching between weather-station-component and historical-average-component
+  // inside the weather-page
+
+  let weatherStationComponent = document.querySelector(
+    ".weather-station-component"
+  );
+  let historicalAverageComponent = document.querySelector(
+    ".historical-average-component"
+  );
+  let weatherStationButton = document.getElementById("weatherStationBtn");
+  let historicalAverageButton = document.getElementById("historicalAverageBt");
+
+  weatherStationButton.addEventListener("click", pageSwitch());
+  historicalAverageButton.addEventListener("click", pageSwitch());
+
+  function pageSwitch() {
+    console.log("page switch was clicked");
+    historicalAverageComponent.classList.toggle("d-none");
+    weatherStationComponent.classList.toggle("d-none");
+  }
+  function triggerHistoricAverage() {}
+  function triggerWeatherStation() {}
+
+  /**
+   * All public or private methods which need to call onInit
+   */
+  const weatherPagePage = document.querySelector("#weather-page-page");
+  if (weatherPagePage !== null) {
+    weatherPagePage.addEventListener("afterLoad", onInit);
+  }
+
+  /**
+   * All public method and properties are exported here
+   */
+  return {
+    getOutput: getOutput,
+  };
+})();
